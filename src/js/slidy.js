4494842ba76dd4b4b61f28c55b10106d084dcfbc
Валентин Немцев <mail@valentin-nemcev.com>
2016-05-04 20:48:31 +0300

    Implement mouse wheel presentation controls

--- src/js/slidy.js
+++ src/js/slidy.js
@@ -6,6 +6,9 @@ import SlidyRouter from './router';
 import SlidyDeck from './deck';
 import clearSelection from './clearSelection';
 
+import jQueryMousewheel from 'jquery-mousewheel';
+import throttle from 'throttle-debounce/throttle';
+
 
 $(document).ready(() => {
   const screen = new SlidyScreen({$screen: $(document.body)});
@@ -21,9 +24,15 @@ $(document).ready(() => {
     function (ev) {
       screen.showSlide($(this));
       ev.preventDefault();
-      clearSelection();
     }
   );
+
+
+  screen.$screen.on('slidy:showSlide', (ev, $slide) => {
+    clearSelection();
+    $slide.scrollTop(0);
+  });
+
 
   $(document).on(
     'keydown',
@@ -45,5 +54,35 @@ $(document).ready(() => {
       }
       return false;
     }
+  );
+
+
+  jQueryMousewheel($);
+
+  $(document).on(
+    'mousewheel',
+    '.presenting .presented',
+    throttle(250, /* noTrailing */ true, (ev) => {
+      const $this = $(ev.currentTarget);
+
+      if (!deck.getOptionsForSlide($this).scrollControl) return;
+      const scrollingDown = ev.deltaY < 0;
+      const scrollingUp = ev.deltaY > 0;
+
+      // http://stackoverflow.com/questions/6271237/detecting-when-user-scrolls-to-bottom-of-div-with-jquery/6271466#6271466
+      // - 1 because innerHeight could be fractional and scrollHeight is
+      // always rounded
+      const scrolledToBottom =
+        $this.scrollTop() + $this.innerHeight() >= $this[0].scrollHeight - 1;
+      const scrolledToTop = $this.scrollTop() === 0;
+
+      if (scrollingDown && scrolledToBottom) {
+        screen.showSlide(deck.getNextSlide(screen.getCurrentSlide()));
+      }
+
+      if (scrollingUp && scrolledToTop) {
+        screen.showSlide(deck.getPrevSlide(screen.getCurrentSlide()));
+      }
+    })
   );
 });
